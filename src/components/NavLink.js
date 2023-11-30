8552140c5e8b93781c03798ece2526eec6aedb4a
Loïc Humbert <loic@humbert-web.dev>
2023-11-30 22:25:11 +0100

    clean

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,14 +1,7 @@
 import {html} from "../html.js";
-import {loadPage} from "../router.js";
 
 export default (link, text) => {
-    const navlink = html`
+    return html`
         <a href="${link || '#'}">${text}</a>
     `;
-    // navlink.content.firstChild.addEventListener('click', e => {
-    //     console.log("click")
-    //     e.preventDefault();
-    //     loadPage(link);
-    // });
-    return navlink;
 }
