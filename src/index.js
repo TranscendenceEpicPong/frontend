6ce65565fd84316c28772714aa0b3c05509a78fb
Loïc Humbert <loic@humbert-web.dev>
2023-11-30 15:19:29 +0100

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,42 +1,53 @@
-document.querySelector('#test').innerText = `${Math.random()}`
+function component() {
+    const element = document.createElement('div');
 
-const router = document.querySelector('#router');
-const routerjs = document.querySelector('#router-js');
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = 'Hello World';
 
-function loadPage(link) {
-    if (link.length === 1)
-        link = 'home';
-    routerjs.setAttribute('src', `${link}.js`);
-    history.pushState({}, {}, link);
-    import(`./${link}.js`)
-        .then(response => {
-            router.innerHTML = response.page()
-        })
-        .catch(error => {
-            if (link === '404')
-                alert('Une erreur est survenue');
-            else
-                loadPage('404');
-        })
+    return element;
 }
 
-const links = document.querySelectorAll('a');
-links.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const href = link.getAttribute('href');
-        loadPage(href);
-    })
-})
+document.body.appendChild(component());
 
-window.addEventListener('load', () => {
-    const path = window.location.pathname
-    history.pushState({}, {}, path);
-    loadPage(path)
-});
-
-window.addEventListener('popstate', () => {
-    const path = window.location.pathname
-    history.pushState({}, {}, path);
-    loadPage(path)
-});
+// document.querySelector('#test').innerText = `${Math.random()}`
+//
+// const router = document.querySelector('#router');
+// const routerjs = document.querySelector('#router-js');
+//
+// function loadPage(link) {
+//     if (link.length === 1)
+//         link = 'home';
+//     routerjs.setAttribute('src', `${link}.js`);
+//     history.pushState({}, {}, link);
+//     import(`./${link}.js`)
+//         .then(response => {
+//             router.innerHTML = response.page()
+//         })
+//         .catch(error => {
+//             if (link === '404')
+//                 alert('Une erreur est survenue');
+//             else
+//                 loadPage('404');
+//         })
+// }
+//
+// const links = document.querySelectorAll('a');
+// links.forEach(link => {
+//     link.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         const href = link.getAttribute('href');
+//         loadPage(href);
+//     })
+// })
+//
+// window.addEventListener('load', () => {
+//     const path = window.location.pathname
+//     history.pushState({}, {}, path);
+//     loadPage(path)
+// });
+//
+// window.addEventListener('popstate', () => {
+//     const path = window.location.pathname
+//     history.pushState({}, {}, path);
+//     loadPage(path)
+// });
